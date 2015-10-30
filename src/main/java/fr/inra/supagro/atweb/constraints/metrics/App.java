bbf31ac05f218941b7634541f1d530ad1c8b4e10
Leandro Lovisolo <leandro@leandro.me>
2015-10-30 10:54:38 +0100

    Initial import.

---
+++ src/main/java/fr/inra/supagro/atweb/constraints/metrics/App.java
@@ -0,0 +1,13 @@
+package fr.inra.supagro.atweb.constraints.metrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
